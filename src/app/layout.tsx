e95372f18347517abb9086304287ab491e29c229
ukasha <ukashamughal3.0@gmail.com>
2026-03-06 01:26:40 +0500

    added meta script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,19 @@ export default function RootLayout({
         {/* End of Tawk.to Script */}
 
         <link rel="icon" href="/favicon-3.png" />
+
+        {/* Meta Pixel Code */}
+        <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1595522894901835');
+          fbq('track', 'PageView');` }} />
+        {/* End Meta Pixel Code */}
       </head>
       <body
         className={`${syne.variable} ${poppins.variable} ${ShadedLarsh.variable} antialiased`}
@@ -71,6 +84,15 @@ export default function RootLayout({
           />
         </noscript>
         {/* End Google Tag Manager (noscript) */}
+
+        {/* Meta Pixel Code (noscript) */}
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1595522894901835&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code (noscript) */}
+
         <Header />
         {children}
         <Footer />
