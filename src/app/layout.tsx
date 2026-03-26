2e592c76612d8c151692a126b78382957d3b63c3
ukasha <ukashamughal3.0@gmail.com>
2026-03-27 00:15:05 +0500

    added msvalidate.01 meta tag in the header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,6 +83,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://www.google-analytics.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://connect.facebook.net" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://embed.tawk.to" />
+        <meta name="msvalidate.01" content="7000FF60495EE20E0D4689F671EDA603" />
         
         {/* Google Tag Manager */}
         <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-ML8Q9N76');` }} />
@@ -150,8 +151,6 @@ export default function RootLayout({
           />
         </noscript>
         {/* End Google Tag Manager (noscript) */}
-
-
 
         {/* Meta Pixel Code (noscript) */}
         <noscript>
