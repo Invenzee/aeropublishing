152fddf16de629ea011b16d21681ffe8f8e8e338
ukasha <ukashamughal3.0@gmail.com>
2026-04-08 22:11:43 +0500

    updated the metadata

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -14,13 +14,13 @@ import type { Swiper as SwiperType } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import PortfolioSection from '../book-publishing-landing-page/PortfolioSection';
-import { Metadata } from 'next';
+// import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: "Our Portfolio – Books Published by Aero Authors",
-  description:
-    "Browse books published by Aero Publishing. See real author success stories, professional designs, and completed projects across multiple genres.",
-};
+// export const metadata: Metadata = {
+//   title: "Our Portfolio – Books Published by Aero Authors",
+//   description:
+//     "Browse books published by Aero Publishing. See real author success stories, professional designs, and completed projects across multiple genres.",
+// };
 
 
 const faqs = [
